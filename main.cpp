22cd641affd251542dcfba02957ab497696e77e6
v.bokk <v.bokk@g.nsu.ru>
2023-09-14 21:49:26 +0700

    first c

--- main.cpp
+++ main.cpp
@@ -1,5 +1,38 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
 
 int main() {
-	std::cout << "ad";
+	struct timespec start, end;
+	clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+
+	/*ifstream input("input.txt");
+	vector<int> arr;
+	int numb;
+
+	while (input >> numb) {
+		arr.push_back(numb);
+	}
+
+	for (int i = 0; i < arr.size() - 1; i++) {
+		for (int j = 0; j < arr.size() - i - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = arr[j];
+			}
+		}
+	}
+
+	input.close();
+
+	clock_gettime(CLOCK_MONOTONIC_RAW, &end);*/
+	int c = 0;
+	for (int i = 0; i < 1000000; i++) {
+		c++;
+	}
+	printf("Time taken: %lf sec.\n",
+		end.tv_sec - start.tv_sec + 0.000000001 * (end.tv_nsec - start.tv_nsec));
 }
