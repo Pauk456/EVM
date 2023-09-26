5f4ff2b83197acb99e7465c09e07e50f9273ebff
Pauk456 <456pauk@mail.ru>
2023-09-26 21:39:40 +0700

    ss

--- createRandomArr.cpp
+++ createRandomArr.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
-	if (argc == 0) {
+	if (argc != 2) {
 		cout << "Error";
 	}
 
