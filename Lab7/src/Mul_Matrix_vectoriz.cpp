1acf721152dd44793a0bc3b2d07626f65f31de49
Pauk456 <456pauk@mail.ru>
2023-11-25 23:09:10 +0700

    a

--- Lab7/src/Mul_Matrix_vectoriz.cpp
+++ Lab7/src/Mul_Matrix_vectoriz.cpp
@@ -134,13 +134,13 @@ Matrix Matrix::operator-(const Matrix& b) const// Можно векторизовать
 Matrix Matrix::operator*(const Matrix& b) const // Можно векторизовать
 {
 	Matrix Mul(N); // i - строка, j - столбец
-	for (int j1 = 0; j1 < N; j1++)
+	for (int row1 = 0; row1 < N; row1++)
 	{
-		for (int j2 = 0; j2 < N; j2++)
+		for (int col2 = 0; col2 < N; col2++)
 		{
-			for (int i1 = 0; i1 < N; i1++)
+			for (int col1 = 0; col1 < N; col1++)
 			{
-				Mul[i1][j2] += (*this)[i1][j1] * b[j1][j2];
+				Mul[row1][col2] += (*this)[row1][col1] * b[col1][col2];
 			}
 		}
 	}
