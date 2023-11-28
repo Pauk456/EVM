aa5e3c5e991d4686f06fd2ee3668a92d8073f27e
Pauk456 <456pauk@mail.ru>
2023-11-28 22:40:50 +0700

    Добавил Blas

---
+++ Lab7/include/Matrix_BLAS.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <vector>
+#include <iostream>
+#include <limits>
+#include <cblas.h>
+
+class Matrix
+{
+private:
+	int N;
+	std::vector< std::vector<float> > matrix;
+private:
+	Matrix calculate_R(Matrix& B);
+	Matrix calculate_B();
+	float sum_max_row();
+	float sum_max_column();
+public:
+	Matrix(int N);
+	void reverse_matrix(int M);
+	Matrix transpose_matrix() const;
+	void to_single();
+
+	// TEST
+	float sum_max_row_TEST() { return sum_max_row(); }
+	float sum_max_column_TEST() { return sum_max_column(); }
+	//
+
+	std::vector<float>& operator[](int row)
+	{
+		return matrix[row];
+	}
+	const std::vector<float>& operator[](int row) const
+	{
+		return matrix[row];
+	}
+	Matrix operator+(const Matrix& b) const;
+	Matrix& operator+=(const Matrix& b);
+	Matrix operator-(const Matrix& b) const;
+	Matrix operator*(const Matrix& b) const;
+	Matrix& operator/(const float b);
+
+	friend std::ostream& operator<<(std::ostream& os, const Matrix& obj);
+};
+
---
+++ Lab7/src/Matrix_BLAS.cpp
@@ -0,0 +1,173 @@
+#include "Matrix_BLAS.h"
+
+Matrix::Matrix(int N)
+{
+	this->N = N;
+	matrix.resize(N, std::vector<float>(N, 0));
+}
+
+void Matrix::reverse_matrix(int M)
+{
+	Matrix B = calculate_B();
+	Matrix R = calculate_R(B);
+	Matrix Rst = R;
+	(*this).to_single();
+	for (int i = 0; i < M; i++)
+	{
+		(*this) += Rst;
+		Rst = Rst * R;
+	}
+	(*this) = (*this) * B;
+}
+
+Matrix Matrix::transpose_matrix() const {
+	Matrix T(N);
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			T[j][i] = (*this)[i][j];
+		}
+	}
+	return T;
+}
+
+void Matrix::to_single()
+{
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			(*this)[i][j] = i == j ? 1 : 0;
+		}
+	}
+}
+
+Matrix Matrix::operator+(const Matrix& b) const
+{
+	Matrix Sum(N);
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			Sum[i][j] = (*this)[i][j] + b[i][j];
+		}
+	}
+	return Sum;
+}
+
+Matrix& Matrix::operator+=(const Matrix& b)
+{
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			(*this)[i][j] = (*this)[i][j] + b[i][j];
+		}
+	}
+	return (*this);
+}
+
+Matrix Matrix::operator-(const Matrix& b) const
+{
+	Matrix Sub(N);
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			Sub[i][j] = (*this)[i][j] - b[i][j];
+		}
+	}
+	return Sub;
+}
+
+Matrix Matrix::operator*(const Matrix& b) const
+{
+	Matrix Mul(N); // i - строка, j - столбец
+	for (int row1 = 0; row1 < N; row1++)
+	{
+		for (int col2 = 0; col2 < N; col2++)
+		{
+			for (int col1 = 0; col1 < N; col1++)
+			{
+				Mul[row1][col2] += (*this)[row1][col1] * b[col1][col2];
+			}
+		}
+	}
+	return Mul;
+}
+
+Matrix& Matrix::operator/(const float b)
+{
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			matrix[i][j] /= b;
+		}
+	}
+	return (*this);
+}
+
+Matrix Matrix::calculate_R(Matrix& B)
+{
+	Matrix I(N);
+	I.to_single();
+	return I - B * (*this);
+}
+
+Matrix Matrix::calculate_B()
+{
+	float A1 = sum_max_column();
+	float A8 = sum_max_row();
+	return (*this).transpose_matrix() / (A1 * A8);
+}
+
+float Matrix::sum_max_row() // A8
+{
+	float max_sum = std::numeric_limits<float>::lowest();
+	for (int i = 0; i < N; i++) // i - строка, j - столобец
+	{
+		float Ai = 0; // сумма i ой строчки
+		for (int j = 0; j < N; j++)
+		{
+			Ai += matrix[i][j];
+		}
+		if (Ai > max_sum)
+		{
+			max_sum = Ai;
+		}
+	}
+	return max_sum;
+}
+
+float Matrix::sum_max_column() // A1
+{
+	float max_sum = std::numeric_limits<float>::lowest();
+	for (int j = 0; j < N; j++) // i - строка, j - столобец
+	{
+		float Aj = 0; // сумма j ого столбца
+		for (int i = 0; i < N; i++)
+		{
+			Aj += matrix[i][j];
+		}
+		if (Aj > max_sum)
+		{
+			max_sum = Aj;
+		}
+	}
+	return max_sum;
+}
+
+std::ostream& operator<<(std::ostream& os, const Matrix& obj)
+{
+	for (int i = 0; i < obj.N; i++)
+	{
+		for (int j = 0; j < obj.N; j++)
+		{
+			os << obj[i][j] << ' ';
+		}
+		os << '\n';
+	}
+	return os;
+}
