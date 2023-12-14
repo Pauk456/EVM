69bd761ecbf74fdb8be8eaaa704af771f8d8bb75
Pauk456 <456pauk@mail.ru>
2023-12-14 16:54:53 +0700

    a

--- Lab8/main.cpp
+++ Lab8/main.cpp
@@ -1,92 +1,74 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <ctime>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-using namespace std;
-
-unsigned long long rdtsc() {
-    unsigned int low, high;
-    asm volatile ("rdtsc" : "=a" (low), "=d" (high));
-    return ((unsigned long long)high << 32) | low;
-}
-
-void direct(vector<int> Array, const long long N)
-{
-    for (long long i = 0; i < N; i++) {
-        Array[i] = i + 1;
+void ascending(int* data, const long long N) {
+    for (long long i = 0; i < N - 1; i++) {
+        data[i] = i + 1;
     }
-    Array[N - 1] = 0;
+    data[N - 1] = 0;
 }
 
-void reverse(vector<int> Array, const long long N) 
-{
-    Array[0] = N - 1;
+
+void descending(int* data, const long long N) {
+    data[0] = N - 1;
     for (long long i = 1; i < N; i++) {
-        Array[i] = i - 1;
+        data[i] = i - 1;
     }
 }
 
-void random(vector<int> Array, const long long N) 
-{
+
+void random(int* data, const long long N) {
     int temp;
     long long i = N;
     srand(time(NULL));
     while (i > 1) {
         i--;
         int j = rand() % i;
-        temp = Array[i];
-        Array[i] = Array[j];
-        Array[j] = temp;
+        temp = data[i];
+        data[i] = data[j];
+        data[j] = temp;
     }
 }
 
-int test(vector<int> Array, const long long N, const int K, 
-    void (*create_function)(vector<int> Array, const long long N), ofstream& out)
-{
-    create_function(Array, N);
+
+int test(int* data, const long long N, const int K, void (*gen)(int* data, const long long N), FILE* out) {
+    gen(data, N);
     int k;
     long long i;
-    for (k = 0, i = 0; i < N; i++) 
-    {
-        k = Array[k];
+    for (k = 0, i = 0; i < N; i++) {
+        k = data[k];
     }
-    //unsigned long long start = __builtin_ia32_rdtsc();
-    unsigned long long start = rdtsc();
-    for (k = 0, i = 0; i < N * K; i++) 
-    {
-        k = Array[k];
+    unsigned long long start = __builtin_ia32_rdtsc();
+    for (k = 0, i = 0; i < N * K; i++) {
+        k = data[k];
     }
-    //unsigned long long end = __builtin_ia32_rdtsc();
-    unsigned long long end = rdtsc();
-    out << " N = " << N * 4 << ", "
-        << "Tick = " << ((end - start)) << '\n';
-         
+    unsigned long long end = __builtin_ia32_rdtsc();
+    fprintf(out, "N = %lld, Tick = %llu\n", N * 4, (end - start) / N / K);
     return k;
 }
 
-int main() 
-{
-    ofstream out("time.txt");
-    if (!out) {
-       return 0;
-    }
+
+int main() {
     const long long Nmin = 1024 / sizeof(int);
     const long long Nmax = 32 * 1024 * 1024LL / sizeof(int);
     const int K = 10;
 
-    vector<int> Array(Nmax);
+    int* data = (int*)malloc(Nmax * sizeof(int));
 
-    for (int N = Nmin; N <= Nmax; N *= 2) 
-    {
-        out << "N = " << N << "\n";
-        out << "direct: ";
-        test(Array, N, K, direct, out);
-        out << "reverse: ";
-        test(Array, N, K, reverse, out);
-        out << "random: ";
-        test(Array, N, K, random, out);
-        out << '\n';
+    FILE* out = fopen("time.txt", "w");
+
+    for (int N = Nmin; N <= Nmax; N *= 2) {
+        fprintf(out, "N = %d\n", N);
+        fprintf(out, "Asc: ");
+        test(data, N, K, ascending, out);
+        fprintf(out, "Desc: ");
+        test(data, N, K, descending, out);
+        fprintf(out, "Random: ");
+        test(data, N, K, random, out);
+        fprintf(out, "\n");
     }
-    out.close();
+    fclose(out);
+    free(data);
+    return 0;
 }
